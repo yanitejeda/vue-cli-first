3482570773dcfff55d4399d85d69f123ad91d1ae
yanitejeda <tejedasp@gmail.com>
2021-11-10 21:47:48 +0100

    completato

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <titolo />
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Titolo from "./components/Titolo.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Titolo
   }
 }
 </script>
---
+++ src/components/Titolo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1> “La mia prima app con Vue CLI!”</h1> 
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
